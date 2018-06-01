0ec2bad8f80b10e5329a043efd8f7baec3b47340
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2018-06-01 11:26:48 +0200

    Prawdopodobne rozwiazanie przesuniecia na osi x

--- dev/js/BackgroundAnimationManager.js
+++ dev/js/BackgroundAnimationManager.js
@@ -53,7 +53,7 @@ class BackgroundAnimationManager {
             array[i] = {
                 actualAngle: 0,
                 velocity: Math.random() * 2,
-                rotationSpeed: Number.parseInt(Math.random() * 10),
+                rotationSpeed: Number.parseFloat(Math.random() * 1).toFixed(2),
                 width: Number.parseInt(Math.random() * 100),
                 x: (Math.random() * this.getWidth()) + 1,
                 // y: this.getHeight(),
@@ -91,13 +91,18 @@ class BackgroundAnimationManager {
             this.containerGraphicContext.beginPath();
             this.containerGraphicContext.rect(val.x, val.y, val.width, val.width);
 
+
+
             // this.containerGraphicContext.rotate(this.squares[i].actualAngle * Math.PI / 180);
 
             // this.containerGraphicContext.translate(this.getWidth() / 2, this.getHeight() / 2);
             // this.containerGraphicContext.translate(this.squares[i].width / 2, this.squares[i].width / 2);
             // this.containerGraphicContext.translate(0.1,0.1);
 
-            this.containerGraphicContext.rotate(0.01 * Math.PI / 180);
+            // this.containerGraphicContext.rotate(0.01 * Math.PI / 180);
+            this.containerGraphicContext.rotate(1 * Math.PI / 180);
+            //  this.containerGraphicContext.translate(0,-.2);
+
 
             this.containerGraphicContext.fillStyle = `rgba(255,255,255,${val.opacity})`;
             this.containerGraphicContext.fill();
@@ -115,10 +120,52 @@ class BackgroundAnimationManager {
     draw() {
 
         this.clearElement();
-        this.drawSquares();
+        // this.drawSquares();
         // this.updateSquaresVelocity();
 
-        window.requestAnimationFrame(this.draw);
+        // window.requestAnimationFrame(this.draw);
+
+        this.containerGraphicContext.beginPath();
+        this.containerGraphicContext.rect(this.squares[1].x, this.squares[1].y, this.squares[1].width, this.squares[1].width);
+        this.containerGraphicContext.fillStyle = `rgba(255,255,255,${this.squares[1].opacity})`;
+        this.containerGraphicContext.fill();
+
+        console.log(this.squares[1].rotationSpeed);
+        console.log(this.squares[1].x);
+        console.log(`-----------------------------------------`);
+
+        ///------------------------------------------------------------------------
+
+        this.containerGraphicContext.beginPath();
+        this.containerGraphicContext.rotate(5 * Math.PI / 180);
+
+        // console.log(this.squares[1].x.toFixed(2));
+        // console.log(this.squares[1].rotationSpeed);
+
+        // console.log(Number.parseFloat(this.squares[1].x.toFixed(2)) + Number.parseFloat(this.squares[1].rotationSpeed));
+        // console.log(typeof Number.parseFloat(this.squares[1].x.toFixed(2)) * Number.parseFloat(Math.cos(5 * Math.PI / 180)));
+        // console.log(typeof Number.parseFloat(this.squares[1].x.toFixed(2)));
+        let x = Number.parseFloat(this.squares[1].x.toFixed(2));
+        let y = Number.parseFloat(this.squares[1].y.toFixed(2));
+        let x2 = Number.parseFloat(Math.cos(5 * Math.PI / 180));
+        let y2 = Number.parseFloat(Math.sin(5 * Math.PI / 180));
+        let xWynik = (x * x2) - (y * y2);
+        let przesuniecieX = x - xWynik;
+        let noweX = x + przesuniecieX;
+
+        // let x2s = Number.parseFloat(Math.sin(5 * Math.PI / 180));
+        // let y2s = Number.parseFloat(Math.cos(5 * Math.PI / 180));
+        // let xWynik2 = (x * x2s) + (y * y2s);
+        // let przesuniecieX2 = x - xWynik2;
+        // let noweX2 = y + przesuniecieX2;
+        // console.log(noweX2);
+
+
+        //albo transform albo przesuniecie przy recie
+        this.containerGraphicContext.rect(noweX, this.squares[1].y, this.squares[1].width, this.squares[1].width);
+
+        this.containerGraphicContext.fillStyle = `rgba(255,255,255,${this.squares[1].opacity})`;
+        this.containerGraphicContext.fill();
 
     }
 
--- dev/js/StartMenuManager.js
+++
@@ -1,1 +0,0 @@
-console.log(`witaj w starcvie`);
